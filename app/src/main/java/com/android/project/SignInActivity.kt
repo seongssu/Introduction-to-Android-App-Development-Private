3b226a28d991b128c067b4cfb69dc3b13230ef61
seongssu <loveforggong@gmail.com>
2023-08-03 22:12:49 +0900

    [FIX] : 🔧 HomActivity로 데이터넘기는 코드 if문 안쪽으로 이동(순서조심)

--- app/src/main/java/com/android/project/SignInActivity.kt
+++ app/src/main/java/com/android/project/SignInActivity.kt
@@ -21,9 +21,9 @@ class SignInActivity : AppCompatActivity() {
             val psw = edit_psw.text.toString()
             val strData =edit_id.text.toString()
             val intent = Intent(this,HomeActivity::class.java)
-            intent.putExtra("dataFromSignInActivity", strData)
             if(!id.isEmpty()&& !psw.isEmpty()){
-                startActivity(intent)
+                intent.putExtra("dataFromSignInActivity", strData)
+                startActivity(intent)                                       //25,26줄 순서 뒤바뀌면 데이터 안넘어감
                 Toast.makeText(this,"로그인 되었습니다.",Toast.LENGTH_SHORT).show()
             } else {
                 Toast.makeText(this,"아이디/비밀번호를 확인해주세요",Toast.LENGTH_SHORT).show()
