16cc55d7ebb1e9e0dc9dc7443933b01c79089fd8
seongssu <loveforggong@gmail.com>
2023-08-03 22:20:53 +0900

    [FIX] : 🔧 명칭수정 및 lv1. 완료

--- app/src/main/java/com/android/project/SignInActivity.kt
+++ app/src/main/java/com/android/project/SignInActivity.kt
@@ -13,23 +13,26 @@ class SignInActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signin)
 
-        val btn_calllognin =findViewById<Button>(R.id.btnlogin)
+        val btn_calllognin = findViewById<Button>(R.id.btnlogin)
         btn_calllognin.setOnClickListener {
             val edit_id = findViewById<EditText>(R.id.editId)
             val edit_psw = findViewById<EditText>(R.id.editTextPassword)
             val id = edit_id.text.toString()
             val psw = edit_psw.text.toString()
-            val strData =edit_id.text.toString()
-            val intent = Intent(this,HomeActivity::class.java)
-            if(!id.isEmpty()&& !psw.isEmpty()){
+            val strData = edit_id.text.toString()
+            val intent = Intent(this, HomeActivity::class.java)
+            if (!id.isEmpty() && !psw.isEmpty()) {
                 intent.putExtra("dataFromSignInActivity", strData)
                 startActivity(intent)                                       //25,26줄 순서 뒤바뀌면 데이터 안넘어감
-                Toast.makeText(this,"로그인 되었습니다.",Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, "로그인 되었습니다.", Toast.LENGTH_SHORT).show()
             } else {
-                Toast.makeText(this,"아이디/비밀번호를 확인해주세요",Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, "아이디/비밀번호를 확인해주세요", Toast.LENGTH_SHORT).show()
             }
-
+        }
+        val btn_calljoin = findViewById<Button>(R.id.btnjoin)
+        btn_calljoin.setOnClickListener {
+            val intent1 = Intent(this, SignUpActivity::class.java)
+            startActivity(intent1)
         }
     }
-
 }
