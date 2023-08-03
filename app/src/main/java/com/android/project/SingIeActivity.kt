97aa7edb75fada59f471a2c9540725c56d8f47c9
seongssu <loveforggong@gmail.com>
2023-08-03 20:28:01 +0900

    [PACK] : 🎁 개인과제 기본 뼈대

---
+++ app/src/main/java/com/android/project/SingIeActivity.kt
@@ -0,0 +1,11 @@
+package com.android.project
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SingIeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_single)
+    }
+}
