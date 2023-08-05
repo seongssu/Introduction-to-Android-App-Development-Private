6d40f6aa45777ccba54dbb01f01d39fcb8f683ca
seongssu <loveforggong@gmail.com>
2023-08-06 08:10:56 +0900

    [FEAT] : ⚙️  Theme -> String 데이터 정리

--- app/src/main/java/com/android/project/SignInActivity.kt
+++ app/src/main/java/com/android/project/SignInActivity.kt
@@ -50,7 +50,7 @@ class SignInActivity : AppCompatActivity() {
                     startActivity(intent1)
                     Toast.makeText(this, "로그인 되었습니다.", Toast.LENGTH_SHORT).show()
                 } else {
-                    Toast.makeText(this, "회원가입 부터 해주세요", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(this, "아이디/비밀번호를 확인해주세요", Toast.LENGTH_SHORT).show()
                 }
             } else {
                 Toast.makeText(this, "아이디/비밀번호를 확인해주세요", Toast.LENGTH_SHORT).show()
